8614be059723dc2ad9eceb70f1ad2edf82821f5a
nam24 <namratatripathi24@gmail.com>
2022-03-14 11:31:39 +0530

    commit

--- populateDB.py
+++ populateDB.py
@@ -1,49 +1,47 @@
 import psycopg2
 
+class PopulateDB:
+    def populateDBFromCSV():
+        #establishing the connection
+        conn = psycopg2.connect(
+        database="finance", user='namrata', host='127.0.0.1', port= '5432'
+        ) # add roles and passwords
 
-#establishing the connection
-conn = psycopg2.connect(
-   database="finance", user='namrata', host='127.0.0.1', port= '5432'
-) # add roles and password
+        #Creating a cursor object using the cursor() method
+        cursor = conn.cursor()
 
+        # drop pre-existing table
+        dropTableQ = "DROP TABLE IF EXISTS mftransactions;"
+        cursor.execute(dropTableQ)
 
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
+        #create MF transactions table
+        createTableQ = '''
+            CREATE TABLE mftransactions(
+                amc VARCHAR(80) NOT NULL,
+                folio VARCHAR(25) NOT NULL,
+                pan VARCHAR(10),
+                scheme VARCHAR(120),
+                advisor VARCHAR(40),
+                isin VARCHAR(15),
+                amfi VARCHAR(8),
+                date DATE NOT NULL,
+                description TEXT,
+                amount FLOAT8 NOT NULL,
+                units FLOAT8,
+                nav FLOAT8,
+                balance FLOAT8,
+                type TEXT,
+                dividend varchar(10)
+            );
+        '''
+        cursor.execute(createTableQ)
 
-#Executing an MYSQL function using the execute() method
+        importCSVQ = "COPY mftransactions FROM '/Users/namrata/Documents/PM/cas.csv' DELIMITER ',' CSV HEADER;"
+        cursor.execute(importCSVQ)
 
-# drop pre-existing table
-dropTableQ = "DROP TABLE IF EXISTS mftransactions;"
-cursor.execute(dropTableQ)
+        #amc, scheme-name, isin, amfi, pan
+        cursor.execute("select * from mftransactions")
+        print(cursor.fetchall())
 
-#create MF transactions table
-createTableQ = '''
-    CREATE TABLE mftransactions(
-        amc VARCHAR(80) NOT NULL,
-        folio VARCHAR(25) NOT NULL,
-        pan VARCHAR(10),
-        scheme VARCHAR(120),
-        advisor VARCHAR(40),
-        isin VARCHAR(15),
-        amfi VARCHAR(8),
-        date DATE NOT NULL,
-        description TEXT,
-        amount FLOAT8 NOT NULL,
-        units FLOAT8,
-        nav FLOAT8,
-        balance FLOAT8,
-        type TEXT,
-        dividend varchar(10)
-    );
-'''
-cursor.execute(createTableQ)
-
-importCSVQ = "COPY mftransactions FROM '/Users/namrata/Documents/PM/cas.csv' DELIMITER ',' CSV HEADER;"
-cursor.execute(importCSVQ)
-
-#amc, scheme-name, isin, amfi, pan
-cursor.execute("select * from mftransactions")
-print(cursor.fetchall())
-
-#Closing the connection
-conn.close()
+        #Closing the connection
+        conn.close()
