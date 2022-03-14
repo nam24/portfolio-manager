47691547ec953f9acfae7c70dfc7487826e02b36
nam24 <namratatripathi24@gmail.com>
2022-03-14 16:00:36 +0530

    add ideas

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ if(refreshMFTransactions):
 # get total amount invested by scheme
 # get total amount invested by AMC
 # get curve of amt invested vs month
+# get % distribution in LC/MC/SC/L&MC...
+# Lumpsum vs SIP
+# try and get number of SIPs and their dates and amounts by code
+# SIP chart - x axis shows dates in a month and y shows amount (find way to show which market cap)
