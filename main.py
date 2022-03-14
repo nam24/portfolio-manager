73c2dce663e486db196b0bedb3fc57a83d79e9d1
nam24 <namratatripathi24@gmail.com>
2022-03-14 13:56:38 +0530

    future commitments

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ from populateDB import PopulateDB
 CreateCSV.createCSVFromPDF()
 sleep(5)
 PopulateDB.populateDBFromCSV()
+
+# get total amount invested
+# get total amount invested by scheme
+# get total amount invested by AMC
+# get curve of amt invested vs month
