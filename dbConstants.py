96c88194886e0d3b30a4cc15014d62b6909e7a1e
nam24 <namratatripathi24@gmail.com>
2022-03-15 18:58:40 +0530

    commit

--- dbConstants.py
+++ dbConstants.py
@@ -6,12 +6,12 @@ class DBConstants:
     mfValues = 'mfvalues'
 
     # Transaction types
-    MISC = 'MISC'
     PURCHASE_SIP = 'PURCHASE_SIP'
     PURCHASE = 'PURCHASE'
-    REDEMPTION = 'REDEMPTION'
-    REVERSAL = 'REVERSAL'
     STAMP_DUTY_TAX = 'STAMP_DUTY_TAX'
+    REVERSAL = 'REVERSAL'
+    MISC = 'MISC'
+    REDEMPTION = 'REDEMPTION'
     STT_TAX = 'STT_TAX'
 
     transactionTypes = [MISC, PURCHASE_SIP, PURCHASE, REDEMPTION, REVERSAL, STAMP_DUTY_TAX, STT_TAX]
