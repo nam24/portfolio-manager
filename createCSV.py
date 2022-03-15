5bcc1a85348a6bb617776d27cc1ddee86b45a4a3
nam24 <namratatripathi24@gmail.com>
2022-03-15 13:40:25 +0530

    commit

--- createCSV.py
+++ createCSV.py
@@ -6,6 +6,6 @@ class CreateCSV:
         switchDirCmd = 'cd ' + DBConstants.filesLocation
         exportFullCSVCmd = 'casparser -o cas.csv -p abcdefgh12 cas.pdf'
         exportSummaryCSVCmd = 'casparser -o cas-summary.csv -p abcdefgh12 -s -a cas.pdf'
-        tell.app( 'Terminal', 'do script "' + switchDirCmd + '"' + 'in window 1') 
-        tell.app( 'Terminal', 'do script "' + exportFullCSVCmd + '"' + 'in window 1') 
-        tell.app( 'Terminal', 'do script "' + exportSummaryCSVCmd + '"' + 'in window 1')
+        tell.app('Terminal', 'do script "' + switchDirCmd + '"' + 'in window 1') 
+        tell.app('Terminal', 'do script "' + exportFullCSVCmd + '"' + 'in window 1') 
+        tell.app('Terminal', 'do script "' + exportSummaryCSVCmd + '"' + 'in window 1')
