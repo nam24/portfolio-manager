5fdc58727a2e95098f5a759e2c5fe80814642a1c
nam24 <namratatripathi24@gmail.com>
2022-05-11 00:09:01 +0530

    commit

--- reports/reports.py
+++ reports/reports.py
@@ -6,54 +6,65 @@ class Reports:
         # 1. Clean up the data
         # 2. Get numbers and print on console/ file
         mfData = ReportHelperFunctions.cleanUpDbObject(mfData)
-        mfInfo = mfData.mfInfo
-        mfValues = mfData.mfValues
         mfTransactions = mfData.mfTransactions
         # adjusting for redemptions
         adjTransactions = ReportHelperFunctions.getRedemptionAdjustedTransactions(mfTransactions)
 
-        print('Without keeping redemptions in mind,')
-        totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.PURCHASE_SIP)
-        print('Total amount invested through SIPs is', round(totalSIP, 3))
-
-        totalLumpSum = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.PURCHASE)
-        print('Total amount invested through lumpsum is', round(totalLumpSum, 3))
-
-        totalStampDuty = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.STAMP_DUTY_TAX)
-        print('Total stamp duty is', round(totalStampDuty, 3))
-
-        totalAmountInvested = ReportHelperFunctions.getCurrentTotalAmt(mfTransactions)
-        print('Total amount invested is', round(totalAmountInvested, 3))
-
-        print()
+        totalNumberOfAMCs = len(set(list(map(lambda x: x.amc, mfTransactions))))
+        print('Total number of fund houses invested in: ', totalNumberOfAMCs, '.')
         print('Adjusting for redemptions,')
-        totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(adjTransactions, Constants.PURCHASE_SIP)
-        print('Total amount invested through SIPs is', round(totalSIP, 3))
-
-        totalLumpSum = ReportHelperFunctions.getTotalAmountByTransactionType(adjTransactions, Constants.PURCHASE)
-        print('Total amount invested through lumpsum is', round(totalLumpSum, 3))
-
-        totalStampDuty = ReportHelperFunctions.getTotalAmountByTransactionType(adjTransactions, Constants.STAMP_DUTY_TAX)
-        print('Total stamp duty is', round(totalStampDuty, 3))
-
         totalAmountInvested = ReportHelperFunctions.getCurrentTotalAmt(adjTransactions)
-        print('Total amount invested is', round(totalAmountInvested, 3))
+        totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(adjTransactions, Constants.PURCHASE_SIP)
+        totalLumpSum = ReportHelperFunctions.getTotalAmountByTransactionType(adjTransactions, Constants.PURCHASE)
+        total = totalSIP + totalLumpSum
+        totalStampDuty = ReportHelperFunctions.getTotalAmountByTransactionType(adjTransactions, Constants.STAMP_DUTY_TAX)
+        print('Total amount invested including stamp duty: ', round(totalAmountInvested, 3), '.')
+        print('Total amount invested through SIPs: ', round(totalSIP, 3), ' (', round(totalSIP*100/total,2), '% ).')
+        print('Total amount invested through lumpsum: ', round(totalLumpSum, 3), ' (', round(totalLumpSum*100/total,2),'% ).')
+        print('Total stamp duty: ', round(totalStampDuty, 3), '.')
+        
 
+        # print('Without keeping redemptions in mind,')
+        # totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.PURCHASE_SIP)
+        # print('Total amount invested through SIPs is', round(totalSIP, 3))
 
-        # get total amount invested
-        # get total amount invested by scheme
-        # get total amount invested by AMC
-        # get curve of amt invested vs month
-        # get % distribution in LC/MC/SC/L&MC...
-        # Lumpsum vs SIP
-        # try and get number of SIPs and their dates and amounts by code
-        # SIP chart - x axis shows dates in a month and y shows amount (find way to show which market cap)
+        # totalLumpSum = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.PURCHASE)
+        # print('Total amount invested through lumpsum is', round(totalLumpSum, 3))
 
-        # print all 3 tables in excel and calculated figures as well
-        # get a txt report
-        # show graphs in report??
+        # totalStampDuty = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.STAMP_DUTY_TAX)
+        # print('Total stamp duty is', round(totalStampDuty, 3))
 
-        # how to treat redemptions?
-        # compare units in folio and scheme subtract from 1st?---> complex
-        # sum all except redemption. Rest of the claculation will require manual input
-        # have another function for redeemed funds
+        # totalAmountInvested = ReportHelperFunctions.getCurrentTotalAmt(mfTransactions)
+        # print('Total amount invested is', round(totalAmountInvested, 3))
+        
+        # Priority
+                # top level summary- no. of amc, funds, folios, folios by fund
+                # get total amount invested by scheme
+                # get total amount invested by AMC
+                # get % distribution in LC/MC/SC/L&MC...
+                # print all 3 tables in excel and calculated figures as well
+                # get a txt report
+                # fetch all long term capital gains purchase transactions
+                # adding sanity checks
+                # testing/ adding checks that csv is correct
+        
+        # Maybe
+                # try and get number of SIPs and their dates and amounts by code
+                # SIP chart - x axis shows dates in a month and y shows amount (find way to show which market cap)
+                # show graphs in report??
+                # get curve of amt invested vs month
+        # Done
+                # get total amount invested
+                # Lumpsum vs SIP
+
+                # how to treat redemptions?
+                # compare units in folio and scheme subtract from 1st?---> complex
+                # sum all except redemption. Rest of the claculation will require manual input
+                # have another function for redeemed funds
+      
+      
+       
+        
+        
+
+       
