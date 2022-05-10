12101d6765e1ed00d2c9a575418e85c2b6adc818
nam24 <namratatripathi24@gmail.com>
2022-05-11 01:41:16 +0530

    commit

--- constants.py
+++ constants.py
@@ -16,3 +16,20 @@ class Constants:
     STT_TAX = 'STT_TAX' # Redemption of Axis Midcap Regular
 
     transactionTypes = [MISC, PURCHASE_SIP, PURCHASE, REDEMPTION, REVERSAL, STAMP_DUTY_TAX, STT_TAX]
+
+    ELSS_FUNDS = [
+        'Quant Tax Plan - Direct Plan',
+        'Canara Robeco Equity Tax Saver Fund - Direct Growth',
+        'Mirae Asset Tax Saver Fund - Direct Growth',
+        'Axis Long Term Equity Fund - Direct Growth'
+    ]
+    FundsByMC = {
+        'LargeCap':[],
+        'MidCap':[],
+        'SmallCap':[],
+        'LargeMidCap':[],
+        'MultiCap':[],
+        'FlexiCap':[],
+        'Focused':[],
+        'ELSS': ELSS_FUNDS
+    }
--- reports/reports.py
+++ reports/reports.py
@@ -10,9 +10,28 @@ class Reports:
         mfTransactions = mfData.mfTransactions
         # adjusting for redemptions. This contains all stamp duties and misc, though
         adjTransactions = ReportHelperFunctions.getRedemptionAdjustedTransactions(mfTransactions)
-        print('Stamp Duty and other fees have not been included in the below numbers (unless specified.\n')
+        print('Stamp Duty and other fees have not been included in the below numbers (unless specified).\n')
         purchaseTransactions = {x for x in adjTransactions if x.type in [Constants.PURCHASE, Constants.PURCHASE_SIP, Constants.REVERSAL]}
         
+        # Get top level fund nav summary
+        Reports.calculateTopLevelNAVSummary(purchaseTransactions)
+        # get market cap summary
+        # get transactions summary
+        print('Adjusting for redemptions,')
+        totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(purchaseTransactions, [Constants.PURCHASE_SIP])
+        totalLumpSum = ReportHelperFunctions.getTotalAmountByTransactionType(purchaseTransactions, [Constants.PURCHASE])
+        total = totalSIP + totalLumpSum
+        totalStampDuty = ReportHelperFunctions.getTotalAmountByTransactionType(
+                            adjTransactions, 
+                            [Constants.STAMP_DUTY_TAX, Constants.MISC, Constants.STT_TAX]
+                        )
+        print('Total amount invested through SIPs: ', round(totalSIP, 2), ' (', round(totalSIP*100/total,2), '% )')
+        print('Total amount invested through lumpsum: ', round(totalLumpSum, 2), ' (', round(totalLumpSum*100/total,2),'% )\n')
+        print('Total other (stamp duty, transaction charges, redemption STT_Tax): ', round(totalStampDuty, 3))
+        
+    def calculateTopLevelNAVSummary(purchaseTransactions):
+        totalAmountInvested = sum(map(lambda x: x.amount, purchaseTransactions))
+        print('Total amount invested: ', round(totalAmountInvested, 2))
         allAMCs = set(list(map(lambda x: x.amc, purchaseTransactions)))
         allFunds = set(list(map(lambda x: x.scheme, purchaseTransactions)))
         allFolios = set(list(map(lambda x: x.folio, purchaseTransactions)))
@@ -29,33 +48,9 @@ class Reports:
             funds = set(list(map(lambda z: z.scheme, amctr)))
             for y in funds:
                 fundtr = {z for z in amctr if z.scheme == y}
-                print(f'    * {y}')
-
-
-
-
-
-
-
-
-
-
-
+                amt = sum(map(lambda z: z.amount, fundtr))  
+                print(f'    * {y}: {round(amt,2)}')
         print()
-        print('Adjusting for redemptions,')
-        totalAmountInvested = sum(map(lambda x: x.amount, purchaseTransactions))
-        totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(purchaseTransactions, [Constants.PURCHASE_SIP])
-        totalLumpSum = ReportHelperFunctions.getTotalAmountByTransactionType(purchaseTransactions, [Constants.PURCHASE])
-        total = totalSIP + totalLumpSum
-        totalStampDuty = ReportHelperFunctions.getTotalAmountByTransactionType(
-                            adjTransactions, 
-                            [Constants.STAMP_DUTY_TAX, Constants.MISC, Constants.STT_TAX]
-                        )
-        print('Total amount invested: ', round(totalAmountInvested, 2))
-        print('Total amount invested through SIPs: ', round(totalSIP, 2), ' (', round(totalSIP*100/total,2), '% )')
-        print('Total amount invested through lumpsum: ', round(totalLumpSum, 2), ' (', round(totalLumpSum*100/total,2),'% )\n')
-        print('Total other (stamp duty, transaction charges, redemption STT_Tax): ', round(totalStampDuty, 3))
-        
 
         # print('Without keeping redemptions in mind,')
         # totalSIP = ReportHelperFunctions.getTotalAmountByTransactionType(mfTransactions, Constants.PURCHASE_SIP)
@@ -71,13 +66,12 @@ class Reports:
         # print('Total amount invested is', round(totalAmountInvested, 3))
         
         # Priority
-                # top level summary- no. of amc, funds, folios, folios by fund
-                # get total amount invested by scheme
-                # get total amount invested by AMC
                 # get % distribution in LC/MC/SC/L&MC...
+                # fetch all long term capital gains eligible purchase transactions
+
                 # print all 3 tables in excel and calculated figures as well
                 # get a txt report
-                # fetch all long term capital gains purchase transactions
+                
                 # adding sanity checks
                 # testing/ adding checks that csv is correct
         
@@ -86,9 +80,13 @@ class Reports:
                 # SIP chart - x axis shows dates in a month and y shows amount (find way to show which market cap)
                 # show graphs in report??
                 # get curve of amt invested vs month
+                # no. of folios by fund
         # Done
                 # get total amount invested
                 # Lumpsum vs SIP
+                # top level summary- no. of amc, funds, folios
+                # get total amount invested by AMC
+                # get total amount invested by scheme
 
                 # how to treat redemptions?
                 # compare units in folio and scheme subtract from 1st?---> complex
